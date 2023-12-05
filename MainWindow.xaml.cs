66c0e545c9fe3f7b35bc9316e49bc6313bb5330f
Olga Bachurina <bachuri12@ukr.net>
2023-12-05 14:47:13 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppAnimation2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeAnimation();
+        }
+
+        private void InitializeAnimation()
+        {
+            Canvas.SetLeft(animatedSquare, 0);
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                By = 760,
+                Duration = TimeSpan.FromSeconds(2),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            Storyboard.SetTarget(animation, animatedSquare);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.LeftProperty));
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+
+            storyboard.Begin(this);
+        }
+    }
+        
+}
+        
+    
+
